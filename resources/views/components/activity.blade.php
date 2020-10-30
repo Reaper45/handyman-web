46af5b8ad98456dbc01fe5fd2525bde322af666e
Joram <jomwashighadi@gmail.com>
2020-10-30 14:24:38 +0300

    fix: job list

--- resources/views/components/activity.blade.php
+++ resources/views/components/activity.blade.php
@@ -72,7 +72,7 @@
                 />
             </div>
             <div class="text-sm leading-5 font-medium text-gray-700 truncate">
-                <b class="ml-1">{{ $job->handyman->name }}</b>
+                <span class="ml-1">{{ $job->handyman->name }}</span>
                 <div class="flex items-center text-sm leading-5 text-gray-500">
                     <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
